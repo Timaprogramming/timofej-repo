de005914565fec9b90370e7df5f756b33a0eda22
Timaprogramming <115730387+Timaprogramming@users.noreply.github.com>
2022-12-03 19:50:58 +0500

    api-expressJS

---
+++ api-expressJS/src/main.ts
@@ -0,0 +1,30 @@
+import { App } from "./app";
+import { ExeptionFilter } from "./errors/exeption.filter";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/user.controller";
+import { Container, ContainerModule, interfaces } from "inversify";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
+import { IExeptionFilter } from "./errors/exeption.filter.interface";
+
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): IBootstrapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
+}
+
+export const { appContainer, app } = bootstrap();
