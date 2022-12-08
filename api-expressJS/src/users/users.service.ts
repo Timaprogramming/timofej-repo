eb29f4aa3ade02aad2d7f0144ee5d4380ba57a81
Timaprogramming <115730387+Timaprogramming@users.noreply.github.com>
2022-12-09 00:24:01 +0500

    Ошибка исправлена

--- api-expressJS/src/users/users.service.ts
+++ api-expressJS/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
 import { User } from "./user.entity";
 import { IUserService } from "./users.service.interface";
+import "reflect-metadata";
 
 @injectable()
 export class UserService implements IUserService {
